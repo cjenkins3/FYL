6fbe64224872a2158d479d4a30818b41b016f33f
Jenkins <cajenkins@smu.edu>
2019-01-24 16:59:24 -0600

    test

---
+++ views/fireBase.js
@@ -0,0 +1,16 @@
+var app_fireBase = {};
+
+(function() {
+      // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyDY29HFVIKQpdLjWB_v07ELFAYgUgrmr7s",
+        authDomain: "finesseyourlife-29290.firebaseapp.com",
+        databaseURL: "https://finesseyourlife-29290.firebaseio.com",
+        projectId: "finesseyourlife-29290",
+        storageBucket: "",
+        messagingSenderId: "556092004449"
+      };
+      firebase.initializeApp(config);
+    
+        app_fireBase = firebase;
+})()
